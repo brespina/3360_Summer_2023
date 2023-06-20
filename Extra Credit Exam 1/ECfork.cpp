dbd28ec7994f05a17456bdad2134db88d2e19cdf
brespina <brespina4@gmail.com>
2023-06-20 16:19:52 -0500

    Added EC folder with a solution

---
+++ Extra Credit Exam 1/ECfork.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+
+    int pid;
+    std::cout << "I am the parent process" << std::endl;
+    for (int i = 0; i < 4; i++) {
+        if((pid = fork()) == 0) {
+            std::cout << "I am child process " << i << std::endl;
+
+            if(i % 3 != 0) {
+                for(int j = 0; j < 2; j++) {
+                    if((pid = fork()) == 0) {
+                        std::cout << "I am a grandchild process from process " << i << std::endl;
+                        _exit(0);
+                    }
+                    wait(nullptr);
+                }
+            }
+            _exit(0);
+        }
+        wait(nullptr);
+    }
+
+    // for(int i = 0; i < 4; i++)
+    //     wait(nullptr);
+
+
+    return 0;
+}
