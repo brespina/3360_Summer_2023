c244afc23bce79681c4853451603030479b873ec
brespina <brespina4@gmail.com>
2023-06-20 16:25:40 -0500

    commented code

--- Extra Credit Exam 1/ECfork.cpp
+++ Extra Credit Exam 1/ECfork.cpp
@@ -1,3 +1,15 @@
+/*
+Brandon Espina
+COSC 3360
+06/20/2023
+PROF RINCON
+EC EXAM 1
+
+NOTE: must call wait() in the same block as fork() is called to guarantee order of output.
+      Ideally, we call wait() in an outside for loop an equal number of times
+      that the parent calls fork() but for moodle's auto grading we must guarantee order of output.
+*/
+
 #include <iostream>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -10,18 +22,18 @@ int main() {
         if((pid = fork()) == 0) {
             std::cout << "I am child process " << i << std::endl;
 
-            if(i % 3 != 0) {
+            if(i % 3 != 0) {  // if i is not 0 or 3
                 for(int j = 0; j < 2; j++) {
                     if((pid = fork()) == 0) {
                         std::cout << "I am a grandchild process from process " << i << std::endl;
                         _exit(0);
                     }
-                    wait(nullptr);
+                    wait(nullptr);  // wait for grandchildren
                 }
             }
             _exit(0);
         }
-        wait(nullptr);
+        wait(nullptr);  // wait for children 
     }
 
     // for(int i = 0; i < 4; i++)
