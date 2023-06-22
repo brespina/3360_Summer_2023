5f2ec306288336a8eabf30cd278eaab061ec0cdd
brespina <brespina4@gmail.com>
2023-06-22 13:14:43 -0500

    remove using namespace std;

--- OS Unit 1/PA1/main.cpp
+++ OS Unit 1/PA1/main.cpp
@@ -16,21 +16,19 @@ NOTE: This program is influenced by Dr. Rincon's UNIT 1 thread examples from CAN
 #include <unistd.h> 
 #include <vector> 
 
-using namespace std;
-
 struct threadData { //defining struct of data passed to thread
-    string inputStr;
-    string rleStr;
-    vector<int> rleFreq; 
+    std::string inputStr;
+    std::string rleStr;
+    std::vector<int> rleFreq; 
 };
 
 //creating void function and manipulating threadData 
 void * rle_encode(void *ptr) { 
     struct threadData * rleData = (struct threadData * ) ptr;  // create object of threadData = to dereferenced void argument ptr
     int count;
-    string str = rleData->inputStr; //threadData input string being fed into str 
-    string rle;
-    vector<int> freq;
+    std::string str = rleData->inputStr; //threadData input string being fed into str 
+    std::string rle;
+    std::vector<int> freq;
 
     //for loop that avoids calling .length
     for (int i = 0; i < str.length(); i++) { 
@@ -58,12 +56,12 @@ void * rle_encode(void *ptr) {
 
 
 int main() {
-    vector<string> strVect;
-    string temp;
+    std::vector<std::string> strVect;
+    std::string temp;
     // read in the input strings from user into vector of strings
-    while(cin >> temp) {
+    while(std::cin >> temp)
         strVect.push_back(temp);
-    };
+
 
 //everything following is influenced by Dr. Rincon's thread example programs provided in CANVAS
 
@@ -71,33 +69,30 @@ int main() {
 
     threadData* x = new threadData[strVect.size()]; 
     pthread_t* tid = new pthread_t[strVect.size()];
-    // vector<threadData> x;
-    // vector<pthread_t> tid;
-    // static struct threadData x[100];
-    // pthread_t tid[100];
 
-
-    	for (int i = 0; i < strVect.size(); i++) {
-            x[i].inputStr = strVect[i];
-            cout << x[i].inputStr << endl;
-             //populate threadData array x with hardcode strArray
-            if (pthread_create(&tid[i], NULL, rle_encode, &x[i])) {  //calling algorithm but also halting if evaluated as TRUE.
-                cerr << "Error creating thread" << endl;
-                return 1;
-            }
-	//	pthread_join(tid[i], NULL);
-	}
+    for (int i = 0; i < strVect.size(); i++) {
+        x[i].inputStr = strVect[i];
+        std::cout << x[i].inputStr << std::endl;
+            //populate threadData array x with hardcode strArray
+        if (pthread_create(&tid[i], NULL, rle_encode, &x[i])) {  //calling algorithm but also halting if evaluated as TRUE.
+            std::cerr << "Error creating thread" << std::endl;
+            return 1;
+        }
+    //	pthread_join(tid[i], NULL);
+    }
 	
-	// Wait for the other threads to finish. Edited
+	// Wait for the other threads to finish.
 	for (int i = 0; i < strVect.size(); i++)
         	pthread_join(tid[i], NULL);
-	for (int i = 0; i < strVect.size(); i++) {  // the NTHREADS - 1 is only because in moodle i need it. 
-        cout << "Input string: " << x[i].inputStr << endl;
-        cout << "RLE String: " << x[i].rleStr << endl;
-        cout << "RLE Frequencies: ";
+
+	for (int i = 0; i < strVect.size(); i++) {  
+        std::cout << "Input string: " << x[i].inputStr << std::endl;
+        std::cout << "RLE String: " << x[i].rleStr << std::endl;
+        std::cout << "RLE Frequencies: ";
         for(int j = 0; j < x[i].rleFreq.size(); j++)  // iterate thru appropriate threadData rleFreq int vector and print
-            cout << x[i].rleFreq[j] << " "; 
-        cout << endl << endl;
+            std::cout << x[i].rleFreq[j] << " ";
+             
+        std::cout << std::endl << std::endl;
     }
 
     // deallocate memory
