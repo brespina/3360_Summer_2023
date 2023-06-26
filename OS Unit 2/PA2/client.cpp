20470838f96d91015c4cac62e6ceb16e325df770
brespina <brespina4@gmail.com>
2023-06-26 15:56:18 -0500

    adding test client and server to PA2

---
+++ OS Unit 2/PA2/client.cpp
@@ -0,0 +1,16 @@
+/*
+Brandon Espina
+06/26/2023
+COSC 3360
+Dr. Rincon
+Programming Assginment 2
+client.cpp
+*/
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World from client" << std::endl;
+    return 0;
+}
---
+++ OS Unit 2/PA2/server.cpp
@@ -0,0 +1,16 @@
+/*
+Brandon Espina
+06/26/2023
+COSC 3360
+Dr. Rincon
+Programming Assginment 2
+server.cpp
+*/
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World from server" << std::endl;
+    return 0;
+}
