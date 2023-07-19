b94074ce996d093ffe45474cd7179b83418d2f2a
brespina <brespina4@gmail.com>
2023-07-19 14:48:03 -0500

    finished PA3 reorganized folders and files.

--- OS Unit 3/PA3/main.cpp
+++ OS Unit 3/PA3/main.cpp
@@ -1,171 +1,154 @@
-// #include <iostream>
-// #include <pthread.h>
-// #include <string>
-
-// struct infoFromMain
-// {
-//     char ch;
-//     pthread_mutex_t *bsem;
-//     pthread_cond_t *printTurn;
-//     int *turn;
-//     int threadID;
-// };
-
-// void *reverse(void *void_ptr)
-// {
-//     infoFromMain ptr = *(infoFromMain *)void_ptr;
-//     pthread_mutex_lock(ptr.bsem);
-//     while (*ptr.turn != ptr.threadID)
-//         pthread_cond_wait(ptr.printTurn, ptr.bsem);
-//     pthread_mutex_unlock(ptr.bsem);
-//     std::cout << ptr.ch;
-//     pthread_mutex_lock(ptr.bsem);
-//     (*ptr.turn)--;
-//     pthread_cond_broadcast(ptr.printTurn);
-//     pthread_mutex_unlock(ptr.bsem);
-
-//     return nullptr;
-// }
-
-// int main()
-// {
-//     pthread_mutex_t bsem;
-//     pthread_cond_t printTurn = PTHREAD_COND_INITIALIZER;
-//     pthread_mutex_init(&bsem, nullptr);
-//     static int turn;
-//     std::string input;
-//     std::cin >> input;
-//     int size = input.size();
-//     turn = size - 1;
-//     pthread_t *tid = new pthread_t[size];
-//     infoFromMain arg;
-//     arg.bsem = &bsem;
-//     arg.printTurn = &printTurn;
-//     arg.turn = &turn;
-//     for (int i = 0; i < size; i++)
-//     {
-//         arg.ch = input[i];
-//         arg.threadID = i;
-//         pthread_create(&tid[i], nullptr, reverse, &arg);
-//     }
-//     for (int i = 0; i < size; i++)
-//     {
-//         pthread_join(tid[i], nullptr);
-//     }
-//     if (tid != nullptr)
-//         delete[] tid;
-//     return 0;
-// }
-
 /*
 Brandon Espina
 COSC 3360
-06/22/2023
+07/19/2023
 PROF RINCON
-PROGRAMMING ASSIGNMENT 1
+PROGRAMMING ASSIGNMENT 3
 */
 
 /*
-NOTE: This program is influenced by Dr. Rincon's UNIT 1 thread examples from CANVAS
-
-need to implement pa1 but instead of using separate memory addresses to pass information to the child threads
-we will be sharing the same shared memory address amongst the child threads and must use semaphores inorder to 
-solve the two synchronization problems. e.g. deal with race condition as well as print in order.
+NOTE: This program is influenced by Dr. Rincon's UNIT 1 thread examples from CANVAS, my own PA1,
+      Dr. Rincon's UNIT 2 Semaphores Folder of example code, namely, mutexsem.cpp,
+      Dr. Rincon's Programming Question on the Summer 2023 Exam 2,
+      Dr. Rincon's "reverse a string" code provided on TEAMS on 07/05/2023,
+      the print and rle_func functions are slightly modified functions from my PA2
 */
 
-#include <iostream> 
-#include <string> 
-#include <pthread.h> 
-#include <unistd.h> 
-#include <vector> 
-#include <semaphore.h>
-#include <semaphore>
+#include <iostream>
+#include <string>
+#include <pthread.h>
+#include <unistd.h>
+#include <vector>
 
-struct threadData { //defining struct of data passed to thread
+struct threadData // defining struct of data to be passed into child from main. modified from PA1
+{
     std::string inputStr;
-    std::string rleStr;
-    std::vector<int> rleFreq; 
-    // add variable for portno to pass into openSock in void function call.
+    int threadID;
+    int *turn;
     pthread_mutex_t *bsem;
-    pthread_cond_t *printTurn;
-    sem_t s;
+    pthread_mutex_t *copy;
+    pthread_cond_t *wait;
 };
 
-//creating void function and manipulating threadData 
-void * rle_encode(void *ptr) { 
-    struct threadData rleData = *(struct threadData * ) ptr;  // create object of threadData = to dereferenced void argument ptr
+// each child thread will execute this concurrently as we only use variables local to the child thread as arguments
+void *rle_func(std::string input, std::string &rleOut, std::vector<int> &freqy)
+{
     int count;
-    //signal(ptr.s);
-    std::string str = rleData.inputStr; //threadData input string being fed into str 
-    std::string rle;
-    std::vector<int> freq;
-    //sem_init(rleData->s, 0);
+    std::string str = input;  // input string received from child thread defined in CS1
 
-    for (int i = 0; i < str.length(); i++) { 
+    for (int i = 0; i < str.length(); i++)
+    {
         count = 1;
-        while (str[i] == str[i + 1]) {   //compare current char with next. if same inc count and i
+        while (str[i] == str[i + 1]) // compare current char with next. if same inc count and i
             count++, i++;
-        }
-        
-        if (count > 1) {   //if count > 1 then run has occured
-            rle += str[i]; 
-            rle += str[i];   // add two instances of that char to RLE string, denoting run.
-            freq.push_back(count);  // populate vector<int> freq with count of repeated char
-        } 
-        else {
-            rle += str[i]; // if no run occured then add 1 instance of that char
-        }
-    }
-    // feed resulting string into threadData object rleData
-    rleData.rleStr = rle;
-    for (int i=0; i<freq.size(); i++) 
-        rleData.rleFreq.push_back(freq[i]); 
 
-    return NULL;
+        if (count > 1)  // if count > 1 then run has occured
+        { 
+            rleOut += str[i];
+            rleOut += str[i];        // add two instances of that char to RLE string, denoting run.
+            freqy.push_back(count);  // populate vector<int> freq with count of repeated char
+        }
+        else
+            rleOut += str[i];  // if no run occured then add 1 instance of that char
+    }
+    return nullptr;
 }
 
+// all variables being printed are local to the respective child thread. Call outside critical section
+void printChild(std::string inputStr, std::string rleStr, std::vector<int> frequency)
+{
 
-int main() {
+    std::cout << "Input string: " << inputStr << std::endl;
+    std::cout << "RLE String: " << rleStr << std::endl;
+    std::cout << "RLE Frequencies: ";
+    for (int j = 0; j < frequency.size(); j++)
+        std::cout << frequency[j] << " ";
+
+    std::cout << std::endl
+              << std::endl;
+}
+
+// creating void function and manipulating threadData
+void *rle_encode(void *ptr)
+{
+    struct threadData rleData = *(struct threadData *)ptr; 
+
+    // ensure that child threads copy the proper ordered instance of shared struct
+    while (*rleData.turn != rleData.threadID)
+        pthread_cond_wait(rleData.wait, rleData.copy);
+
+    // atomically copying instance of shared struct into local variables
+    std::string str = rleData.inputStr;
+    int threadNum = rleData.threadID;
+
+    pthread_cond_broadcast(rleData.wait);
+    pthread_mutex_unlock(rleData.copy); // child unlocks the lock from parent
+/*---------------------------END of CS1---------------------------*/
+
+    std::string rle;
+    std::vector<int> freq;
+
+    rle_func(str, rle, freq);  // rle_function is concurrently being executed across each child
+
+/*--------------------------START of CS2--------------------------*/
+    // all children wait turn to print
+    pthread_mutex_lock(rleData.bsem);
+    while (*rleData.turn != threadNum)
+        pthread_cond_wait(rleData.wait, rleData.bsem);
+    pthread_mutex_unlock(rleData.bsem);
+/*---------------------------END of CS2---------------------------*/
+
+    printChild(str, rle, freq);  
+
+/*--------------------------START of CS3--------------------------*/
+    // after print increment turn
+    pthread_mutex_lock(rleData.bsem);
+    (*rleData.turn)++;
+    pthread_cond_broadcast(rleData.wait);
+    pthread_mutex_unlock(rleData.bsem);
+/*---------------------------END of CS3---------------------------*/
+    return nullptr;
+}
+
+int main()
+{
+    pthread_mutex_t bsem;
+    pthread_mutex_t copy;
+    pthread_cond_t wait = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_init(&bsem, nullptr);
+    pthread_mutex_init(&copy, nullptr);
+    static int turn;
+    turn = 0;
+
     std::vector<std::string> strVect;
     std::string temp;
-    // read in the input strings from user into vector of strings
-    while(std::cin >> temp)
+    //  read in the input strings from moodle into vector of strings
+    while (std::cin >> temp)
         strVect.push_back(temp);
 
+    pthread_t *tid = new pthread_t[strVect.size()];
+    threadData x;  // requirements restrict our data container to one memory address that must be shared
 
-//everything following is heavily influenced by Dr. Rincon's thread example programs provided in CANVAS
-
-    //declaring dynamic for input string x. and POSIX threads.
-    threadData x;
-    pthread_t* tid = new pthread_t[strVect.size()];
-    sem_init(x.s, 0);
-    for (int i = 0; i < strVect.size(); i++) {
+    for (int i = 0; i < strVect.size(); i++)
+    {
+/*-------------------------START of CS1-------------------------*/
+        pthread_mutex_lock(&copy);
         x.inputStr = strVect[i];
+        x.threadID = i;
+        x.turn = &turn;
+        x.bsem = &bsem;
+        x.wait = &wait;
+        x.copy = &copy;
 
-        if (pthread_create(&tid[i], NULL, rle_encode, &x)) {  //calling algorithm but also halting if evaluated as TRUE.
-            std::cerr << "Error creating thread" << std::endl;
-            return 1;
-        }
-        sem_wait(x.s);
-    //	pthread_join(tid[i], NULL);
-    }
-	
-	// Wait for the other threads to finish.
-	for (int i = 0; i < strVect.size(); i++)
-        	pthread_join(tid[i], NULL);
-
-	for (int i = 0; i < strVect.size(); i++) {  
-        std::cout << "Input string: " << x[i].inputStr << std::endl;
-        std::cout << "RLE String: " << x[i].rleStr << std::endl;
-        std::cout << "RLE Frequencies: ";
-        for(int j = 0; j < x[i].rleFreq.size(); j++)  // iterate thru appropriate threadData rleFreq int vector and print
-            std::cout << x[i].rleFreq[j] << " ";
-
-        std::cout << std::endl << std::endl;
+        pthread_create(&tid[i], nullptr, rle_encode, &x);  // CS1 will end in the child thread after data is copied into local variables
     }
 
-    // deallocate memory
-    delete[] x;
-    delete[] tid;
-	return 0;
+    for (int i = 0; i < strVect.size(); i++)
+    {
+        pthread_join(tid[i], nullptr);
+    }
+
+    if (tid != nullptr)
+        delete[] tid;
+    return 0;
 }
